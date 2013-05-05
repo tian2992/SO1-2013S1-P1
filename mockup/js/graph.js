3b8a1b3143ff288d984caf81834647835e0fa392
Sebastian Oliva <tian2992@gmail.com>
2013-05-05 16:57:30 -0600

    added chart

--- mockup/js/graph.js
+++ mockup/js/graph.js
@@ -1,23 +1,21 @@
  d3.json('data.json', function(data) {
-  nv.addGraph(function() {
-    var chart = nv.models.stackedAreaChart()
-                  .x(function(d) { return d[0] })
-                  .y(function(d) { return d[1] })
-                  .clipEdge(true);
+   nv.addGraph(function() {
+     var chart = nv.models.stackedAreaChart()
+                   .x(function(d) { return d[0] })
+                   .y(function(d) { return d[1] })
+                   .clipEdge(true);
 
-    chart.xAxis
-        .showMaxMin(false)
-        .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+     chart.xAxis
+         .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
 
-    chart.yAxis
-        .tickFormat(d3.format(',.2f'));
+     chart.yAxis
+         .tickFormat(d3.format(',.2f'));
 
-    d3.select('#chart svg')
-      .datum(data)
-        .transition().duration(500).call(chart);
+     d3.select('#chart svg')
+       .datum(data)
+         .transition().duration(500).call(chart);
+     nv.utils.windowResize(chart.update);
 
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-  });
+     return chart;
+   });
  })
