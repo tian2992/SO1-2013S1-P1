2c13155ceccb9e6edeb4341ebe57efb3d6ac17a9
Sebastian Oliva <tian2992@gmail.com>
2013-05-13 09:49:20 -0600

    modifications to server.js

--- mockup/server.js
+++ mockup/server.js
@@ -6,33 +6,29 @@ var express = require('express')
 	,io = require('socket.io').listen(server)
 	,votos = require('./data.json');
 
-
 //var mongolianServer = new mongolian;
-	
-
 
 app.set("view options", {layout: false});
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/',function(req,res){
 	res.render('main.html');
 });
 
-
 io.sockets.on('connection', function(socket){
-	//A los nuevos usuarios les envia todos los datos previamente cargados, 
+	//A los nuevos usuarios les envia todos los datos previamente cargados,
 	//los datos se leen desde la base de datos
-	
 	socket.emit('connect',votos);
 
 	var i = 0;
 	setInterval(function(){
 		//TODO: Enviar unicamente cuando encuentre cambios en la base de datos
 		//Actualizar data.json si existen cambios
-		//Se envia el nombre del partido y el numero de votos	
+		//Se envia el nombre del partido y el numero de votos
 		socket.emit('update', 'PP' , ++i);
 	}, 1000);
 });
 
-
-server.listen(80);
+server.listen(8080);
