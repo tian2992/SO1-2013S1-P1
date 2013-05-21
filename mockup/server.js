feb7ed2ec08cb2ac665802fcf5705738da1cc650
Juan Pablo Escobedo <juanp766@gmail.com>
2013-05-21 16:06:52 -0600

    Lee de la base de datos una unica vez por todos los clientes pero manda toda la informacion.

--- mockup/server.js
+++ mockup/server.js
@@ -1,5 +1,5 @@
-//TODO: limpiar codigo
-//TODO: descargar mongolian de git porque el codigo de npm no esta actualizado
+//TODO: ordenar y optimizar codigo
+//TODO: descargar mongolian de github porque el codigo de npm no esta actualizado
 var express = require('express')
 	,mongolian = require("mongolian")
 	,http = require('http')
@@ -24,86 +24,128 @@ app.get('/',function(req,res){
 
 
 //Estructura de los votos y departamentos
-var votos = [{	"key" : "Cuenta de Votos",
+var votos = [{	"key" : "Total",
 		"values" : []
 	    }];
-
 var deptos = [];
 
 //carga los departamentos
 collection.group({ns:"votos",
-		  key:{Location:1},
-		  initial:{},
-		  $reduce:function(curr,result){}}, 
-		  function(error,post){
-			deptos = post.retval;
-		  }
+	  key:{Location:1},
+	  initial:{},
+	  $reduce:function(curr,result){}}, 
+	  function(error,post){
+		for(var i=0;i<post.retval.length;i++){
+			votos.push({"key":post.retval[i].Location, "values":[]});
+			deptos.push(post.retval[i].Location);
+		}
+	  }
 );
 
-//carga los partidos y su cuenta de votos
+//carga los partidos y su cuenta de votos total
 collection.group({ns:"votos",
+	key:{"candidato.partido":1},
+	initial:{total:0},
+	$reduce:function(curr,result){result.total+=1;}}, 
+	function(error,post){
+		for(var i=0;i<post.retval.length;i++){
+			votos[0].values.push({"label":post.retval[i]['candidato.partido'],"value":post.retval[i].total});
+		}
+	  }
+);
+
+//carga los partidos y su cuenta de votos por departamento
+collection.group({ns:"votos",
+	key:{"candidato.partido":1,"Location":1},
+	initial:{total:0},
+	$reduce:function(curr,result){result.total+=1;}}, 
+	function(error,post){
+		for(var i=0;i<post.retval.length;i++){
+			for(var j=0;j<votos.length;j++){
+				if(votos[j].key == post.retval[i].Location){
+					votos[j].values.push({"label":post.retval[i]['candidato.partido'],"value":post.retval[i].total});
+					break;
+				}
+			}
+		}
+	  }
+);
+
+
+//Actualiza variables con datos de la base de datos, cada segundo
+var interval = setInterval(function(){
+	//actualiza los partidos y su cuenta de votos total
+	collection.group({ns:"votos",
 		key:{"candidato.partido":1},
 		initial:{total:0},
 		$reduce:function(curr,result){result.total+=1;}}, 
 		function(error,post){
 			for(var i=0;i<post.retval.length;i++){
-				votos[0].values.push({"label":post.retval[i]['candidato.partido'],"value":post.retval[i].total});
+				var partidoEncontrado = false;
+				for(var j=0;j<votos[0].values.length;j++){
+					if(votos[0].values[j].label == post.retval[i]['candidato.partido']){
+						votos[0].values[j].value = post.retval[i].total;
+						partidoEncontrado = true;
+						break;
+					}				
+				}
+				if(!partidoEncontrado){
+					votos[0].values.push({"label":post.retval[i]['candidato.partido'],"value":post.retval[i].total});
+				}			
 			}
 		  }
-);
+	);
 
+	//actualiza los partidos y su cuenta de votos por departamento
+	collection.group({ns:"votos",
+		key:{"candidato.partido":1,"Location":1},
+		initial:{total:0},
+		$reduce:function(curr,result){result.total+=1;}}, 
+		function(error,post){
+			for(var i=0;i<post.retval.length;i++){
+				var deptoEncontrado = false;
+				for(var j=0;j<votos.length;j++){
+					if(votos[j].key == post.retval[i].Location){
+						var partidoEncontrado = false;
+						for(var k=0;k<votos[j].values.length;k++){
+							if(votos[j].values[k].label == post.retval[i]['candidato.partido']){
+								votos[j].values[k].value = post.retval[i].total;
+								partidoEncontrado = true;
+								break;
+							}
+						}
+						if(!partidoEncontrado){
+							votos[j].values.push({"label":post.retval[i]['candidato.partido'],"value":post.retval[i].total});
+						}
+						deptoEncontrado = true;
+						break;
+					}
+				}
+				if(!deptoEncontrado){
+					deptos.push(post.retval[i].Location);
+					votos.push({"key":post.retval[i].Location,"values":[{"label":post.retval[i]['candidato.partido'],"value":post.retval[i].total}]});				
+				}
+			}
+		  }
+	);
+}, 1000);
 
 io.sockets.on('connection', function(socket){
 	var depto;
-	
 	//A los nuevos usuarios les envia todos los datos previamente cargados
-	socket.emit('connect',votos,deptos);
+	socket.emit('connect',[votos[0]],deptos);
 	
+	//Cada cierto tiempo envia todos los datos, si el usuario se encuentra viendo un departamento envia datos solo del departamento.
 	var interval = setInterval(function(){
-		//Enviar unicamente cuando encuentre cambios en la base de datos, revisa cada segundo
-		//Se envia el nombre del partido y el numero de votos
-		//TODO: no actualiza todos los partidos, cambia variable global para todos los clientes.
-		if(depto){
-			collection.group({ns:"votos",
-				key:{"candidato.partido":1},
-				initial:{total:0},
-				cond:{"Location":depto},
-				$reduce:function(curr,result){result.total+=1;}}, 
-				function(error,post){
-					for(var i = 0;i<post.retval.length;i++){
-						for(var j=0;j<votos[0].values.length;j++){
-							if(votos[0].values[j].label == post.retval[i]['candidato.partido']){
-								if(votos[0].values[j].value != post.retval[i].total){
-									votos[0].values[j].value = post.retval[i].total;
-									socket.volatile.emit('update',post.retval[i]['candidato.partido'],post.retval[i].total);	
-								}
-								break;
-							}
-						}
-					}
-				}
-			);
+		if(!depto){
+			socket.volatile.emit('connect',[votos[0]],deptos);
 		}else{
-			collection.group({ns:"votos",
-				key:{"candidato.partido":1},
-				initial:{total:0},
-				$reduce:function(curr,result){result.total+=1;}}, 
-				function(error,post){
-					for(var i = 0;i<post.retval.length;i++){
-						for(var j=0;j<votos[0].values.length;j++){
-							if(votos[0].values[j].label == post.retval[i]['candidato.partido']){
-								if(votos[0].values[j].value != post.retval[i].total){
-									votos[0].values[j].value = post.retval[i].total;
-									socket.volatile.emit('update',post.retval[i]['candidato.partido'],post.retval[i].total);	
-								}
-								break;
-							}
-						}
-					}
+			for(var i=0; i<votos.length; i++){
+				if(depto == votos[i].key){
+					socket.volatile.emit('connect',[votos[i]],deptos);
 				}
-			);
+			}
 		}
-
 	}, 1000);
 
 	socket.on('disconnect', function () {
